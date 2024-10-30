ed5d6accda3a76e2d8152da42a15f1e7f313335c
Yusuf Tör <3296904+yusuftor@users.noreply.github.com>
2024-10-30 22:38:15 +0100

    swiftlint fixes

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "Superscript",
-      targets: ["Superscript"]),
+      targets: ["Superscript"]
+    )
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -31,6 +32,7 @@ let package = Package(
     ),
     .testTarget(
       name: "SuperscriptTests",
-      dependencies: ["Superscript"]),
+      dependencies: ["Superscript"]
+    )
   ]
 )
